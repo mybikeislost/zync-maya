2578c2513e88dbe7869044c7536d375ad5c204d5
Brian Cipriano <bcipriano@zyncrender.com>
2012-07-19 12:34:17 -0400

    Fixed issue with menu creation. Tweak UI layout.

--- userSetup.py
+++ userSetup.py
@@ -12,6 +12,6 @@ def create_zync_shelf():
     shelfTab = maya.mel.eval('global string $gShelfTopLevel;')
     maya.mel.eval('global string $scriptsShelf;')
     maya.mel.eval('$scriptsShelf = `shelfLayout -p $gShelfTopLevel ZYNC`;')
-    maya.mel.eval('shelfButton -parent $scriptsShelf -annotation "Render on ZYNC" -label "Render on ZYNC" -image "zync.png" -sourceType "python" -command ("maya_zync.submit_dialog()") -width 34 -height 34 -style "iconOnly";')
+    maya.mel.eval('shelfButton -parent $scriptsShelf -annotation "Render on ZYNC" -label "Render on ZYNC" -image "zync.png" -sourceType "python" -command ("zync_maya.submit_dialog()") -width 34 -height 34 -style "iconOnly";')
 
 maya.utils.executeDeferred( create_zync_shelf )
